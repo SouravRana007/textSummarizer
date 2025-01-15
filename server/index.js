4ee7df400674cfc8d33a4fa47903b445f4c560d2
Sourav Rana <souravraghav001@gmail.com>
2025-01-15 11:46:11 +0530

    database connection and login route is added

--- server/index.js
+++ server/index.js
@@ -1,26 +1,46 @@
 import express from "express";
 import User from "./models/user.models.js";
+import { connectDB } from "./Database/db.js";
 import bcrypt from "bcrypt";
 
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4001;
 app.use(express.json());
+
+// connecting mongoDB with the server
+connectDB()
+  .then(() => {
+    app.on("Error", (error) => {
+      console.error("Error : ", error);
+      throw error;
+    });
+
+    // console.log("Port: ", PORT);
+    app.listen(PORT, () => {
+      console.log(`Server is listening at : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO_DB Connection Failed : ", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 //register api
 app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
   try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ error: "User with this email already exists" });
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
       name,
